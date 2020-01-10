c6e14c09704a17d7c2ff15d6b8ece12c98cc13b1
jendhordejan <jendhordejan@gmail.com>
2020-01-10 12:25:27 +0100

    Set up Form Validation

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,33 +1,43 @@
-//Javascript to run the detail-page.js and show the comments submitted
-//by the user.
+// add event handler
+function submitComment() {
+  // gather data
+  const inputField = document.getElementById("name");
+  const name = inputField.value;
+  const textArea = document.getElementById("msg");
+  const msg = textArea.value;
+  // create the elements you need
+  const comment = document.createElement("section");
+  const h3 = document.createElement("h3");
+  const p = document.createElement("p");
+  // adjust the elements we created
+  h3.innerHTML = `${name} said:`;
+  p.innerHTML = msg;
+  comment.classList.add("comment");
+  comment.appendChild(h3);
+  comment.appendChild(p);
+  // display the elements on the page
+  const commentSection = document.getElementById("comments");
+  commentSection.appendChild(comment);
+  // reset form values
+  
+  if (doesNotPassAllValidations(name, msg)){
+    return null;
+  }
 
-<script src="detail-page.js"></script>
-
-function submitComment(){
-  //extract the Name of the user and store it to the variable inputField.
-  const inputField = document.getElementById("name")
-  //assigning the value of inputField to the variable name.
-  const name = inputField.nodeValue
-
-  //extract the comment of the user and store it to the variable textArea.
-  const textArea = document.getElementById("msg")
-  //assigning the value of textArea to the variable 'msg'.
-  const msg = textArea.nodeValue
-
-  //create a new <section> Element and store it in the variable comment.
-  const comment = document.createElement('section')
-
-  //create a new <h3> Element and store it in the variable h3.
-  const h3 = document.createElement("h3")
-
-  //create a new <p> Element and store it in the variable p.
-  const p = document.createElement("p")
-
-  //reassign the values of variables comment, h3, and p to their innerHTML property.
-  p3.innerHTML = `${name} said:`
-  h3.innerHTML = msg
-  comment.classList.add('comment')
-  comment.appendChild(h3)
-  comment.appendChild(p)
-  console.log('comment')
+  inputField.value = null;
+  textArea.value = null;
 }
+
+function doesNotPassAllValidations(name,msg){
+  if (!name || !msg){
+    alert('You forgot to fill in your name or message!')
+    return true;
+  }
+
+  if (msg>280){
+    alert('Your comment is too long')
+    return true;
+  }
+  return false;
+}
+  
